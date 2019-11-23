a8a09f638d383b5479c1e2c089239ac795b807a1
Marcus Norwood <marcusnorwood@MacBook-Pro-2.local>
2019-11-22 20:41:22 -0700

    Converted remaining components and pages to styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import { CartItemContainer, ItemDetailsContainer, NameContainer } from './cart-item.styles';
 
 const CartItem = ({item: {imageUrl, price, name, quantity} }) => (
-  <div className='cart-item'>
+  <CartItemContainer>
     <img src={imageUrl} alt='item' />
-    <div className='item-details'>
-      <div className='name'>{name}</div>
+    <ItemDetailsContainer>
+      <NameContainer>{name}</NameContainer>
       <div className='price'>
        {quantity} x ${price}
       </div>
-    </div>
-  </div>
+    </ItemDetailsContainer>
+  </CartItemContainer>
 );
 
 export default CartItem;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
-import { auth } from 'firebase';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state ={
+    this.state = {
       email: '',
       password: ''
     };
@@ -23,7 +27,7 @@ class SignIn extends React.Component {
     const { email, password } = this.state;
 
     try {
-      await auth().signInWithEmailAndPassword(email, password);
+      await auth.signInWithEmailAndPassword(email, password);
       this.setState({ email: '', password: '' });
     } catch (error) {
       console.log(error);
@@ -37,36 +41,36 @@ class SignIn extends React.Component {
   };
 
   render() {
-    return(
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
+    return (
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name="email"
-            type="email"
+            name='email'
+            type='email'
             handleChange={this.handleChange}
             value={this.state.email}
             label='email'
             required
           />
           <FormInput
-            name="password"
-            type="password"
+            name='password'
+            type='password'
             value={this.state.password}
             handleChange={this.handleChange}
             label='password'
             required
           />
-          <div className='buttons'>
-            <CustomButton type="submit"> SIGN IN </CustomButton>
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              SIGN IN WITH GOOGLE
+              Sign in with Google
             </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
